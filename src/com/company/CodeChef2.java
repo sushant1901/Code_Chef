ccb8a626f3eae72194abd859911c1226718c54aa
Sushant Mishra <mishrasushant1901@gmail.com>
2022-01-10 22:15:57 +0530

    Helping Chef

---
+++ src/com/company/CodeChef2.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef2
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {   Scanner sc= new Scanner(System.in);
+
+        int T= sc.nextInt();
+        while(T-->0){
+            int n= sc.nextInt();
+            if(n<10){
+                System.out.println("Thanks for helping Chef!");
+
+            }else{
+                System.out.println("-1");
+            }
+        }
+    }
+}
