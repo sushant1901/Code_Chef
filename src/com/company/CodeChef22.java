4f0a6d9368dd8881469074a8d6ce75fa0bd4a9f5
Sushant Mishra <mishrasushant1901@gmail.com>
2022-01-30 22:34:39 +0530

    Lapindromes

---
+++ src/com/company/CodeChef22.java
@@ -0,0 +1,35 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef22
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t--!=0)
+        {
+            String S = sc.next();
+            int len = S.length();
+            String half1="",half2="";
+            int c=0;
+            half1=S.substring(0,len/2);
+            if(len%2==0) half2=S.substring(len/2);
+            else if(len%2!=0) half2=S.substring(len/2+1);
+            char h1[] = half1.toCharArray();
+            char h2[] = half2.toCharArray();
+            Arrays.sort(h1);
+            Arrays.sort(h2);
+            String a = String.copyValueOf(h1);
+            String b = String.copyValueOf(h2);
+
+            if(a.equals(b)) System.out.println("YES");
+            else System.out.println("NO");
+        }
+        sc.close();
+    }
+}
