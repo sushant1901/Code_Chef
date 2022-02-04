6f8b531030c6a4e9cd7ad37f6993e2b1f1fc1885
Sushant Mishra <mishrasushant1901@gmail.com>
2022-02-04 21:35:46 +0530

    N queen puzzle

---
+++ src/com/company/Codechef33.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef33
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc= new Scanner(System.in);
+        int t= sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            double f=Math.pow(0.143*n,n);
+            System.out.println(Math.round(f));
+
+
+        }
+    }
+}
