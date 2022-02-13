20ee2bc72b6b07d49c5743ee57c2a0234291e1f0
Sushant Mishra <mishrasushant1901@gmail.com>
2022-02-13 13:06:11 +0530

    avoid fixed point

---
+++ src/com/company/Codechef37.java
@@ -0,0 +1,30 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef37
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc= new Scanner(System.in);
+        int t= sc.nextInt();
+        while(t-->0){
+            int  n= sc.nextInt();
+            int [] arr= new int[n];
+            for (int i=0;i<n;i++){
+                arr[i]= sc.nextInt();
+            }
+            int k=0;
+            for(int i=0;i<n;i++){
+                int v=i+1+k;
+                if(v== arr[i]){
+                    k++;
+                }
+            }
+            System.out.println(k);
+        }
+    }
+}
