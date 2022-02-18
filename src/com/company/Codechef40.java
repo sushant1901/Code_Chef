dab2fb57f1fb13059b35b99c138d28d770ab5435
Sushant Mishra <mishrasushant1901@gmail.com>
2022-02-18 08:48:01 +0530

    Chef and NoteBook

---
+++ src/com/company/Codechef40.java
@@ -0,0 +1,40 @@
+package com.company;
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef40
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc= new Scanner(System.in);
+        int t= sc.nextInt();
+        while(t-->0){
+            int x= sc.nextInt();
+            int y=sc.nextInt();
+            int k= sc.nextInt();
+            int n=sc.nextInt();
+            int p;
+            int c;
+            int z=x-y;
+            int f=0;
+            for(int i=0;i<n;i++){
+                p=sc.nextInt();
+                c= sc.nextInt();
+                if(p>=z && c<=k){
+                    f=1;
+                }
+            }
+            if(f==1){
+                System.out.println("LuckyChef");
+            }else{
+                System.out.println("UnluckyChef");
+            }
+
+        }
+    }
+}
