1299ce5d6e30b8057f148c33d2500a101d3fe76f
Sushant Mishra <mishrasushant1901@gmail.com>
2022-03-10 20:28:21 +0530

    tyre problem

---
+++ src/com/company/CodeChef42.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc= new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n= sc.nextInt();
+            int m= sc.nextInt();
+            System.out.println((n*2)+(m*4));
+
+        }
+    }
+}
