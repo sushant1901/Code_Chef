7d3ead88e3fb07f7c0f9fe970cdc87f700b48dc2
Sushant Mishra <mishrasushant1901@gmail.com>
2022-03-14 22:23:15 +0530

    Akash and  function(March Long)

---
+++ src/com/company/CodeChef47.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class CodeChef47
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc= new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int a=sc.nextInt();
+            a++;
+            System.out.println(a/2);
+        }
+    }
+}
