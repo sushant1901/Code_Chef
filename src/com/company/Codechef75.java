d0533ba1f446818f8b3fcb0d9fade5646195ee7e
Sushant Mishra <mishrasushant1901@gmail.com>
2022-04-08 22:54:19 +0530

    ezio and guards

---
+++ src/com/company/Codechef75.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef75
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            if(x>=y) System.out.println("yes");
+            else System.out.println("no");
+
+
+        }
+    }
+}
