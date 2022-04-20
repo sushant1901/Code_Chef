c97e6461503b4da5e163c96b7bea1ff36ce84b1a
Sushant Mishra <mishrasushant1901@gmail.com>
2022-04-20 23:12:51 +0530

    Water Requirement

---
+++ src/com/company/CodeChef88.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CodeChef88
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t  = sc.nextInt();
+        while(t-->0){
+            int n =sc.nextInt();
+            System.out.println(2*n);
+        }
+    }
+}
