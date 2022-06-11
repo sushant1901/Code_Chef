db60a4fe3b642be37c11647f329ddde2206ebce2
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-06-11 15:01:40 +0530

    Alternate addition

---
+++ src/com/company/Codechef88.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc= new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    if(a==b) System.out.println("yes");
+		    int diff=(b-a)%3;
+		    if(diff==0 || diff==1)System.out.println("yes");
+		    else System.out.println("no");
+		}
+	}
+}
