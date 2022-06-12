6dcfdbf4deadcf62cda0031ee721169e171458eb
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-06-12 23:08:18 +0530

    Alternate Addition

--- src/com/company/Codechef88.java
+++ src/com/company/Codechef88.java
@@ -7,7 +7,6 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
 		Scanner sc= new Scanner(System.in);
 		int t=sc.nextInt();
 		while(t-->0){
