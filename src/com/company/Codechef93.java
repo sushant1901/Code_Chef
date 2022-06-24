1b59e9e3827afe449bd1ca2a1bb207e9e5d214fa
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-06-24 19:26:04 +0530

    Count the ACs

---
+++ src/com/company/Codechef93.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int p=sc.nextInt();
+		    int ans=p/100;
+		    int r=p%100;
+		    if((r+ans)<=10){
+		        System.out.println(r+ans);
+		    }else{
+		        System.out.println("-1");
+		    }
+		}
+	}
+}
