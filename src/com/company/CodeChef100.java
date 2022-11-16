bb3ead952e040721bd16b58af33484aa6f52e309
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-11-16 22:14:26 +0530

    parity

---
+++ src/com/company/CodeChef100.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes he
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    
+		    if(n % 2 == 0){
+		        System.out.println("Yes");
+		    }else{
+		        System.out.println("No");
+		    }
+		}
+	}
+}
