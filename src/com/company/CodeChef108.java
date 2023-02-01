1b1a9e3329761b35507d9d1fee1b2566a7e6c940
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2023-02-01 23:03:14 +0530

    Redistributing Chocolates

---
+++ src/com/company/CodeChef108.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc= new Scanner(System.in);
+		int taraSutaria= sc.nextInt();
+		while(taraSutaria-->0){
+		    int dishaPatani= sc.nextInt();
+		    int katrina= sc.nextInt();
+		    int rashmika= sc.nextInt();
+		    
+		    int jod=dishaPatani+katrina+rashmika;
+		    if(jod>=6){
+		        System.out.println("yes");
+		    }else{
+		        System.out.println("no");
+		    }
+		    
+		    
+		}
+	}
+}
